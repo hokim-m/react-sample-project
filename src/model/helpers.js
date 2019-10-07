7c11867834ed956bd4b756cdf8de656447cce4b6
Khokim Mamarasulov <alfacms.hm@gmail.com>
2019-10-07 21:40:30 +0500

    Added sorting method

--- src/model/helpers.js
+++ src/model/helpers.js
@@ -1,3 +1,10 @@
 export function array_chunks(array, chunk_size) {
         return Array(Math.ceil(array.length / chunk_size)).fill().map((_, index) => index * chunk_size).map(begin => array.slice(begin, begin + chunk_size));
 }
+
+export function sortArrayWithKey(array, key) {
+        const sort = (a, b) => {
+                return a[key] > b[key] ? 1 : -1;
+        };
+        return array.slice().sort(sort);
+}
