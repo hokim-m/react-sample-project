94e9dddc131dac8217bd9433a0ecffc1ca82948b
Khokim Mamarasulov <alfacms.hm@gmail.com>
2019-10-07 20:45:32 +0500

    Created Companies Component

---
+++ src/controller/companiesController.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class CompaniesController extends React.Component {
+        constructor(props) {
+                super();
+                this.state = {};
+        }
+
+
+        render() {
+                return <div>
+
+                </div>;
+        }
+}
+
+export default CompaniesController;
